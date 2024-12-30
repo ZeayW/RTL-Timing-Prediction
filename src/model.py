2f0f0e70e8f363b39d35cf215616829c6f71ad25
ZeayW <48197878+ZeayW@users.noreply.github.com>
2024-12-30 15:50:58 -0600

    Update model.py

--- src/model.py
+++ src/model.py
@@ -105,40 +105,40 @@ class TimeConv(nn.Module):
             self.mlp_neigh = MLP(hidden_dim, int(hidden_dim / 2), hidden_dim)
         else:
             if self.agg_choice==0:
-                neigh_dim_m = 1 + self.infeat_dim2 + 1
-                neigh_dim_g = 1 + self.infeat_dim1
+                neigh_dim_m = hidden_dim + self.infeat_dim2 + 1
+                neigh_dim_g = hidden_dim + self.infeat_dim1
             else:
-                neigh_dim_m = 1
-                neigh_dim_g = 1
+                neigh_dim_m = hidden_dim
+                neigh_dim_g = hidden_dim
             if self.flag_delay_pi:
                 neigh_dim_m += 1
                 neigh_dim_g += 1
             if self.flag_delay_pd:
                 neigh_dim_m += 1
                 neigh_dim_g += 1
-            self.mlp_neigh_module = MLP(neigh_dim_m, int(hidden_dim / 2), hidden_dim,1)
-            self.mlp_neigh_gate = MLP(neigh_dim_g, int(hidden_dim / 2), hidden_dim,1)
+            self.mlp_neigh_module = MLP(neigh_dim_m, int(hidden_dim / 2), hidden_dim)
+            self.mlp_neigh_gate = MLP(neigh_dim_g, int(hidden_dim / 2), hidden_dim)
         if flag_global:
             self.mlp_global = MLP(1, int(hidden_dim / 2), hidden_dim)
             out_dim = hidden_dim * 2
         if flag_attn:
             hidden_dim_attn = int(hidden_dim/8)
-            atnn_dim_m = 3
-            atnn_dim_g = 1
-            self.mlp_type = MLP(self.infeat_dim2, hidden_dim_attn, hidden_dim_attn,1)
+            atnn_dim_m = hidden_dim + hidden_dim_attn *2
+            atnn_dim_g = hidden_dim
+            self.mlp_type = MLP(self.infeat_dim2, hidden_dim_attn, hidden_dim_attn)
             if self.flag_width:
-                self.mlp_pos = MLP(2, hidden_dim_attn, hidden_dim_attn,1)
+                self.mlp_pos = MLP(2, hidden_dim_attn, hidden_dim_attn)
             else:
-                self.mlp_pos = MLP(1, hidden_dim_attn, hidden_dim_attn,1)
+                self.mlp_pos = MLP(1, hidden_dim_attn, hidden_dim_attn)
             if self.flag_delay_m:
-                atnn_dim_m += 1
-                self.mlp_level_m = MLP(2, hidden_dim_attn, hidden_dim_attn,1)
+                atnn_dim_m += hidden_dim_attn
+                self.mlp_level_m = MLP(2, hidden_dim_attn, hidden_dim_attn)
             if self.flag_delay_g:
-                atnn_dim_g += 1
-                self.mlp_level = MLP(2, hidden_dim_attn, hidden_dim_attn,1)
+                atnn_dim_g += hidden_dim_attn
+                self.mlp_level = MLP(2, hidden_dim_attn, hidden_dim_attn)
             if self.flag_ntype_g:
-                atnn_dim_g += 1
-                self.mlp_type_g = MLP(self.infeat_dim1, hidden_dim_attn, hidden_dim_attn,1)
+                atnn_dim_g += hidden_dim_attn
+                self.mlp_type_g = MLP(self.infeat_dim1, hidden_dim_attn, hidden_dim_attn)
 
             self.attention_vector_g = nn.Parameter(th.randn(atnn_dim_g, 1), requires_grad=True)
             self.attention_vector_m = nn.Parameter(th.randn(atnn_dim_m,1),requires_grad=True)
@@ -161,7 +161,7 @@ class TimeConv(nn.Module):
             h = self.mlp_neigh(nodes.data['neigh']) + self.mlp_self(nodes.data[self.feat_name1])
         # apply activation except the POs
         mask = nodes.data['is_po'].squeeze() != 1
-        #h[mask] = self.activation(h[mask])
+        h[mask] = self.activation(h[mask])
 
         return {'h':h}
 
@@ -180,7 +180,7 @@ class TimeConv(nn.Module):
         else:
             h = self.mlp_neigh_module(nodes.data['neigh']) + self.mlp_self_module(m_self)
 
-        #h[mask] = self.activation(h[mask])
+        h[mask] = self.activation(h[mask])
 
         if self.flag_reverse:
             return {'h':h,'attn_sum':nodes.data['attn_sum'],'attn_max': nodes.data['attn_max']}
@@ -199,7 +199,7 @@ class TimeConv(nn.Module):
             h = self.mlp_neigh_gate(h)
         else:
             h = self.mlp_neigh_gate(nodes.data['neigh']) + self.mlp_self_gate(m_self)
-        #h[mask] = self.activation(h[mask])
+        h[mask] = self.activation(h[mask])
         if self.flag_reverse and self.attn_choice == 1:
             return {'h': h, 'exp_src_sum': nodes.data['exp_src_sum'], 'exp_src_max': nodes.data['exp_src_max']}
         elif self.flag_reverse and self.attn_choice == 0:
@@ -476,8 +476,7 @@ class TimeConv(nn.Module):
             else:
                 h = h_gnn
 
-            rst=h
-            #rst = self.mlp_out(h)
+            rst = self.mlp_out(h)
 
             if not self.flag_train and self.flag_path_supervise:
                 return rst,0
@@ -494,7 +493,7 @@ class TimeConv(nn.Module):
                     graph.ndata['hp'] = nodes_prob
                     graph.ndata['id'] = th.zeros((graph.number_of_nodes(), 1), dtype=th.int64).to(device)
                     graph.ndata['id'][POs] = th.tensor(range(len(POs)), dtype=th.int64).unsqueeze(-1).to(device)
-                    graph.pull(POs, self.message_func_loss, fn.sum('ml', 'loss'), etype='pi2po')
+                    graph.pull(POs, self.message_func_loss, fn.max('ml', 'loss'), etype='pi2po')
                     # print([get_nodename(graph_info['nodes_name'],po) for po in POs])
                     # PIs_mask = graph.ndata['is_pi'] == 1
                     # PIs = th.tensor(range(graph.number_of_nodes())).to(device)[graph.ndata['is_pi'] == 1]
